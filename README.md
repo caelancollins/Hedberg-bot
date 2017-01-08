6915bddd6b0fdfb901fb543232de196561883b98
Caelan Collins <caelan.p.collins@vanderbilt.edu>
2017-01-08 14:37:46 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Hedberg-bot
-A GroupMe bot that produces Mitch Hedberg's best jokes when called 
+A GroupMe bot that produces Mitch Hedberg's best jokes when "yo mitch" is sent
+
+### How To:
+
+* Create bot at https://dev.groupme.com/bots
+* Create Heroku app and deploy project
+* Go to app settings in Heroku and create Config Vars variable
+* Name variable BOT_ID and set variable to bot ID taken from the GroupMe Bots developer page
+
+Based on Pete McGrath's [Tutorial] (https://github.com/groupme/bot-tutorial-nodejs]https://github.com/groupme/bot-tutorial-nodejs)
---
+++ bot.js
@@ -0,0 +1,67 @@
+
+var fs = require('fs');
+var HTTPS = require('https');
+var fileName = 'mitch.txt';
+
+var botID = process.env.BOT_ID;
+
+function respond() {
+  var request = JSON.parse(this.req.chunks[0]),
+      botRegex = /^\yo mitch$/;
+
+  if(request.text && botRegex.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage();
+    this.res.end();
+  } else {
+    console.log("don't care");
+    this.res.writeHead(200);
+    this.res.end();
+  }
+}
+
+function getLine() {
+  var array = fs.readFileSync(fileName).toString().split('\n');
+  var myrandom = Math.floor((Math.random() * array.length) + 1);
+  var mitchLine = array[myrandom];
+  return mitchLine.toString();
+}
+
+
+function postMessage() {
+  var speech, botResponse, options, body, botReq;
+
+  botResponse = getLine();
+
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
+
+
+exports.respond = respond;
